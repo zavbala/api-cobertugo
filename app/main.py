8f2016dbbed1f67b3c866c61a4780a6de365b30a
Jeremy Zabala <jeremy.zbala@gmail.com>
2023-09-05 10:19:53 -0600

    fix: origins

--- app/main.py
+++ app/main.py
@@ -4,18 +4,19 @@ from app.api.routes.api import router
 
 app = FastAPI()
 
-def create_app():
 
+def create_app():
     app.add_middleware(
         CORSMiddleware,
         allow_headers=["*"],
         allow_credentials=True,
         allow_methods=["GET", "POST"],
-        allow_origins=["http://localhost:3000"],
+        allow_origins=["http://localhost:3000", "https://cobertugo.vercel.app"],
     )
 
     app.include_router(router)
 
     return app
+
 
 app = create_app()
